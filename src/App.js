25a1b60f4221794a1f143c05d372f4a632634abe
David Biles <dlbiles1994@gmail.com>
2020-01-07 15:31:12 -0600

    first push, working progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { createGlobalStyle } from 'styled-components';
 
-function App() {
+import Nav from './Components/nav/nav';
+import Footer from './Components/footer/footer';
+import routes from './router';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Backsplash>
+            <span />
+          </Backsplash>
+          <GlobalStyle />
+          <Container>
+            <Nav />
+            {routes}
+            <Footer />
+          </Container>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+
+    ::selection {
+      background: rgba(146, 206, 182, .6);
+    }
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+  }
+`;
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { ROUTE } from './assets/routes';
+
+const routeMap = Object.keys(ROUTE).map((el, i) => {
+  return <Route key={i} exact path={el === 'Home' ? '/' : `/${el}`} component={ROUTE[el]} />
+})
+
+export default (
+  <Switch>
+    {routeMap}
+  </Switch>
+)
